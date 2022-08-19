339c4d68ee0f28f47b79325da5012ad918122d47
mh07607 <mh07607@st.habib.edu.pk>
2022-08-19 09:28:30 +0500

    basic character movement

--- Gawala/Assets/Scripts/player/PlayerMovement.cs
+++ Gawala/Assets/Scripts/player/PlayerMovement.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
+    private PlayerController pc;
+    private CharacterController cc;
+    private Vector3 direction;
+    public int speed = 500;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Movement(cc);
     }
+
+    private void Movement(CharacterController cc)
+    {
+        pc = cc.GetComponent<PlayerController>();
+        direction = new Vector3(0, 0, 1);
+        cc.SimpleMove(direction * speed * Time.deltaTime);
+    }
+
 }
