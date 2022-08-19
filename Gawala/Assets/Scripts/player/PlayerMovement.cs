08cbcb9155803b96ff90c1cc956dcdb38221c1a4
mh07607 <mh07607@st.habib.edu.pk>
2022-08-19 22:07:22 +0500

    intro to bike physics

--- Gawala/Assets/Scripts/player/PlayerMovement.cs
+++ Gawala/Assets/Scripts/player/PlayerMovement.cs
@@ -8,6 +8,12 @@ public class PlayerMovement : MonoBehaviour
     private PlayerController pc;
     private CharacterController cc;
     private Vector3 direction;
+    private float horizontal, vertical;
+
+    [SerializeField] float bikespeed = 500;
+    [SerializeField] WheelCollider frontwheelcoll, backwheelcoll;
+    [SerializeField] Transform frontwheel, backwheel;
+    
     public int speed = 500;
     // Start is called before the first frame update
     void Start()
@@ -18,9 +24,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Movement(cc);
+        //Movement(cc);
+        backwheelcoll.motorTorque = bikespeed ;
+        //frontwheelcoll.motorTorque = bikespeed;
     }
 
+    private void GetInput() //using this as subsitute for player controller
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+
+    }
+
+    
     private void Movement(CharacterController cc)
     {
         pc = cc.GetComponent<PlayerController>();
